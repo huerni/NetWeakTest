82666240aee2e3dff5b5954261afe35ad2248854
huerni <47264950+huerni@users.noreply.github.com>
2025-08-07 09:42:09 +0800

    refactor

--- NetWeakTest.go
+++ NetWeakTest.go
@@ -5,23 +5,26 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	log "github.com/sirupsen/logrus"
 	"os"
 	"os/signal"
 	"strings"
 	"sync"
 	"syscall"
 	"time"
+
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
-	log.SetLevel(log.DebugLevel)
 
-	nodeListStr := flag.String("nodeList", "", "input node list")
+	nodeListStr := flag.String("N", "", "input node list")
 	options := flag.String("options", "", "input options")
-	sleepTime := flag.Duration("time", 30, "input time")
+	sleepTime := flag.Duration("T", 30, "input time")
+	logLevel := flag.String("L", "info", "log level")
 
 	flag.Parse()
+
+	util.InitLogger(*logLevel)
 
 	if *nodeListStr == "" {
 		log.Error("input node list is empty")
@@ -29,85 +32,99 @@ func main() {
 		os.Exit(1)
 	}
 
-	nodePidMap, err := util.GetPidWithNodeNames(strings.Split(*nodeListStr, ","))
-	if err != nil {
-		log.Fatalf(err.Error())
+	nodeList, err := util.ParseHostList(*nodeListStr)
+	if !err {
+		log.Error("parse node list error: %v", err)
+		flag.Usage()
+		os.Exit(1)
 	}
 
-	ctx, cancel := context.WithCancel(context.Background())
+	args := flag.Args()
+	if len(args) == 0 {
+		args = []string{"bash"}
+	}
 
+	done := make(chan struct{})
+
+	go util.RunCmd(args, done)
+
+	ctx, cancel := context.WithCancel(context.Background())
 	wg := sync.WaitGroup{}
-	for nodeName, nodePid := range nodePidMap {
+	defer func() {
+		cancel()
+		wg.Wait()
+		log.Info("exiting.....")
+		// TODO: 检测miniet，存在时stop mininet
+	}()
+
+	for _, nodeName := range nodeList {
 		wg.Add(1)
-		go func(ctx context.Context, nodeName string, nodePid string, options string, sleepTime time.Duration) {
+		go func(ctx context.Context, nodeName string, options string, sleepTime time.Duration) {
 			defer wg.Done()
-			log.Debugf("[%s] Starting Network weak", nodeName)
+			nodePid, err := util.GetPidWithNodeName(ctx, nodeName)
+			if err != nil {
+				if ctx.Err() == context.Canceled || strings.Contains(err.Error(), "signal: interrupt") {
+					return
+				}
+				log.Errorf("[%s] %s", nodeName, err.Error())
+				return
+			}
+
+			log.Infof("[%s] Starting Network weak", nodeName)
+
 			for {
 				select {
 				case <-ctx.Done():
-					log.Debugf("[%s] Network weak stopping", nodeName)
-					cmdStr := fmt.Sprintf("sudo mnexec -a %s tc qdisc del dev %s-eth0 root", nodePid, nodeName)
-					err = util.ExecBashCmd(cmdStr)
-					if err != nil {
-						log.Errorf("[%s] %s", nodeName, err.Error())
-						return
-					}
+					log.Infof("[%s] Network weak stopping...", nodeName)
 					return
-				default:
+				case <-time.After(sleepTime * time.Second):
 					var actualOption string
 					if options != "" {
 						actualOption = options
 					} else {
 						actualOption = util.GetRandomOption()
 					}
-					cmdStr := fmt.Sprintf("sudo mnexec -a %s tc qdisc replace dev %s-eth0 root netem %s", nodePid, nodeName, actualOption)
-					err := util.ExecBashCmd(cmdStr)
-					if err != nil {
-						log.Errorf("[%s] %s", nodeName, err.Error())
-						return
-					}
 
-					cmdStr = fmt.Sprintf("sudo mnexec -a %s tc qdisc show dev %s-eth0", nodePid, nodeName)
+					cmdStr := fmt.Sprintf("sudo mnexec -a %s tc qdisc replace dev %s-eth0 root netem %s", nodePid, nodeName, actualOption)
 					err = util.ExecBashCmd(cmdStr)
 					if err != nil {
-						log.Errorf("[%s] %s", nodeName, err.Error())
-						return
-					}
-
-					select {
-					case <-ctx.Done():
-						log.Debugf("[%s] Network weak stopping", nodeName)
-						cmdStr := fmt.Sprintf("sudo mnexec -a %s tc qdisc del dev %s-eth0 root", nodePid, nodeName)
-						err = util.ExecBashCmd(cmdStr)
-						if err != nil {
-							log.Errorf("[%s] %s", nodeName, err.Error())
+						if ctx.Err() == context.Canceled || strings.Contains(err.Error(), "signal: interrupt") {
 							return
 						}
-						return
-					case <-time.After(sleepTime * time.Second):
+						log.Errorf("[%s] %s", nodeName, err.Error())
 					}
+
+					if log.GetLevel() == log.DebugLevel {
+						cmdStr = fmt.Sprintf("sudo mnexec -a %s tc qdisc show dev %s-eth0", nodePid, nodeName)
+						err = util.ExecBashCmd(cmdStr)
+						if err != nil {
+							log.Infof("[%s] Network weak stopping...", nodeName)
+							if ctx.Err() == context.Canceled || strings.Contains(err.Error(), "signal: interrupt") {
+								return
+							}
+							log.Errorf("[%s] %s", nodeName, err.Error())
+						}
+					}
+
+					//select {
+					//case <-ctx.Done():
+					//	log.Infof("[%s] Network weak stopping...", nodeName)
+					//	return
+					//case <-time.After(sleepTime * time.Second):
+					//}
 				}
 			}
-		}(ctx, nodeName, nodePid, *options, *sleepTime)
-	}
 
-	done := make(chan struct{})
-	go func() {
-		wg.Wait()
-		close(done)
-	}()
+		}(ctx, nodeName, *options, *sleepTime)
+	}
 
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
 	select {
 	case <-done:
-		log.Info("something went wrong")
+
 	case sig := <-sigs:
 		log.Info("receive signal:", sig)
-		cancel()
-		wg.Wait()
 	}
-
-	close(sigs)
 }
--- go.mod
+++ go.mod
@@ -3,14 +3,10 @@ module CraneNetWeak
 go 1.24
 
 require (
+	github.com/antonfisher/nested-logrus-formatter v1.3.1
+	github.com/creack/pty v1.1.24
 	github.com/sirupsen/logrus v1.9.3
-	google.golang.org/grpc v1.74.2
-	google.golang.org/protobuf v1.36.6
+	golang.org/x/term v0.32.0
 )
 
-require (
-	golang.org/x/net v0.40.0 // indirect
-	golang.org/x/sys v0.33.0 // indirect
-	golang.org/x/text v0.25.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20250528174236-200df99c418a // indirect
-)
+require golang.org/x/sys v0.33.0 // indirect
